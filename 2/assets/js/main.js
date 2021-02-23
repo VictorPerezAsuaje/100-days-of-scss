9fd1291c732f28d7c8406534d174c02ff110860e
Victor Pérez Asuaje <v.perezasuaje@gmail.com>
2021-02-23 23:07:55 +0100

    changes added to value with range change

--- 2/assets/js/main.js
+++ 2/assets/js/main.js
@@ -1,7 +1,3 @@
-function ChangeValue(value){
-    document.getElementById('card-value').innerHTML = "$"+ value +".00";
-}
-
 const switcher = document.getElementById('switch');
 const modeChanger = document.getElementById('mode-changer');
 let isSwitched = false;
@@ -10,13 +6,32 @@ switcher.addEventListener('click', ()=>{
     if(!isSwitched){
       switcher.classList.add('clicked');
       modeChanger.classList.add('clicked');
+      switcher.style.backgroundColor = '#51e4ab';
+      modeChanger.style.borderColor = '#51e4ab';
+
       document.getElementById('card__value-type').innerHTML = "  / year";
+      
+      document.getElementById('card-value').innerHTML *= 8;
+
       isSwitched = true;
     }
     else{
       switcher.classList.remove('clicked');
       modeChanger.classList.remove('clicked');
+      switcher.style.backgroundColor = '#cfcfcf';
+      modeChanger.style.borderColor = '#cfcfcf';
       document.getElementById('card__value-type').innerHTML = " / month";
+      document.getElementById('card-value').innerHTML /= 8;
       isSwitched = false;
     }
+
   });
+
+function ChangeValue(value){
+    if(isSwitched){
+        value = value*8;
+    }
+
+    document.getElementById('card-value').innerHTML = value;
+    
+}
