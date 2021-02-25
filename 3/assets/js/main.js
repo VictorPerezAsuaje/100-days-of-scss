24965ab7c38553969eb9b6e7950503640b0a2004
Victor Pérez Asuaje <v.perezasuaje@gmail.com>
2021-02-25 08:40:51 +0100

    Animation added for the image, changes for the color picker and button changes onclick

--- 3/assets/js/main.js
+++ 3/assets/js/main.js
@@ -1,9 +1,21 @@
 function ChangeColor(element){
     const circles = document.querySelectorAll(".selected");
+    const image = document.getElementById('shoe');
 
     for(i = 0; i<circles.length;i++){
         circles[i].classList.remove('selected');
     }
     
     element.classList.add('selected');
+    if(element.id == "black"){
+        image.src = "images/jordan-zoom-92-black-nobg.png";
+    }
+    else if(element.id == "blue"){
+        image.src = "images/jordan-zoom-92-blackblue-nobg.png";
+    }
+    else{
+        image.src  = "images/jordan-zoom-92-zapatillas-blackred-nobg.png";
+    }
+
+
 }
