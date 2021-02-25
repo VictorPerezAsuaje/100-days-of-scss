f6548781c020eb6363541cc8f0376fe2c6cac746
Victor Pérez Asuaje <v.perezasuaje@gmail.com>
2021-02-25 11:24:30 +0100

    Improvements on Javascript - thank you Abdelmonaem Shahat for your insights :) -

--- 3/assets/js/main.js
+++ 3/assets/js/main.js
@@ -2,20 +2,20 @@ function ChangeColor(element){
     const circles = document.querySelectorAll(".selected");
     const image = document.getElementById('shoe');
 
-    for(i = 0; i<circles.length;i++){
-        circles[i].classList.remove('selected');
-    }
+    circles.forEach(item => item.classList.remove('selected'));
     
     element.classList.add('selected');
-    if(element.id == "black"){
-        image.src = "images/jordan-zoom-92-black-nobg.png";
-    }
-    else if(element.id == "blue"){
-        image.src = "images/jordan-zoom-92-blackblue-nobg.png";
-    }
-    else{
-        image.src  = "images/jordan-zoom-92-zapatillas-blackred-nobg.png";
-    }
 
+    switch(element.id){
+        case "black":
+            image.src = "images/jordan-zoom-92-black-nobg.png";
+            break;
+        case "blue":
+            image.src = "images/jordan-zoom-92-blackblue-nobg.png";
+            break;
+        case "red":
+            image.src  = "images/jordan-zoom-92-zapatillas-blackred-nobg.png";
+            break;
+    }
 
 }
